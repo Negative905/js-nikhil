bdea9b61485749fa5a2bfa51b087d568d8b426ff
Nikhil Chauhan <nikhilchauhan6619@gmail.com>
2025-10-22 10:24:19 +0000

    04_comparision.js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+//console.log( 2 < 1);
+//console.log( 2 >= 1);
+//console.log( 2 > 1 );
+//console.log( 2 == 1);
+//console.log( 2 != 1);
+
+
+console.log( "2" > 1 );
+console.log( "02" > 1 );
+
+
+console.log( null > 2);
+console.log( null == 2);
+console.log( null >= 2);
+
+
+// console.log( undefined == 2 );
+// console.log( undefined > 2 );
+// console.log( undefined < 2 );
+
+console.log( "2" === 2 );
+
+
+
+
