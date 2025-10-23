55ea80f89749a1ff2fe5e8f73e51c522cb91d4a0
Nikhil Chauhan <nikhilchauhan6619@gmail.com>
2025-10-23 10:18:02 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// primitive
+
+//  7 types : String, number, boolean, null, undefined, Symbol, BigInt
+
+const value = 100
+const scorevalue = 100.5
+
+const isLoggedIn = false
+const outsidetemp = null
+
+let userEmail;
+
+const id = Symbol ('123')
+const Anotherid = Symbol ('123')
+
+console.log( id === Anotherid )
+
+const bignumber = 13181722424511157458n
+
+// non primitive
+
+// 3 types : Array, Objects, functions
+
+const heroes = [ "leaonard", "sheldon", "howard" ];
+
+let myobj = {
+    name: "nikhil",
+    age: 22,
+}
+
+const myfucntion = function (){
+console.log("hello world");
+    } 
+ 
+console.log( typeof big);
+
+
+
+
