a32094f556f69408eb8e6c2216c3f360590bd6e6
Nikhil Chauhan <nikhilchauhan6619@gmail.com>
2025-11-14 08:13:42 +0000

    01 functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+
+
+function sayMyName(){
+    console.log("N");
+    console.log("I");
+    console.log("K");
+    console.log("H");
+    console.log("I");
+    console.log("L");
+}
+
+// sayMyName()
+
+function addtwonumbers(){
+    
+}
