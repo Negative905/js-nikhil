a37461398189b2b190556bc64944e930715c451f
Nikhil Chauhan <nikhilchauhan6619@gmail.com>
2026-01-10 09:18:25 +0000

    one js 04

---
+++ 04_control_flow/one.js
@@ -0,0 +1,51 @@
+// IF statement
+const isUserLoggedIn = true
+const temperature = 19
+
+
+if ( temperature === 19 ){
+   console.log("less than 20");
+   
+} else {
+ console.log("temperature is greater than 50");
+}
+// console.log("execute");
+// <, >, <=, >=, ==, !=, ===
+
+const score = 200
+if (score > 100) {
+ const power = "fly"
+//   console.log(`user power ${power}`);
+}
+
+
+const balance = 1000
+ if (balance > 500) console.log("test");
+
+ if (balance < 500) {
+    console.log("less than 500")
+} else if (balance < 750){
+   console.log("less than 750");
+   
+} else if (balance < 900) {
+    console.log("less than 900");
+
+} else if (balance > 1200) {
+    console.log("less than 1200");
+    
+}
+
+const UserLoggedIn = true
+const debitcard = true
+const loggedinfromgoogle = false
+const loggedinfromEmail = true
+
+if (UserLoggedIn && debitcard && 2==2)  {
+    console.log("allowed to buy");
+    
+} else if ( loggedinfromgoogle || loggedinfromEmail) {
+    console.log("user logged in");
+}
+
+ 
+ 
