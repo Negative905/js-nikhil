7187267d7dbe87d1329f9c431ea6a251fdf11ae5
Nikhil Chauhan <nikhilchauhan6619@gmail.com>
2026-01-29 18:17:47 +0000

    variable 01

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 916659
+let accountEmail = "negative@google.com"
+var accountPassword = "90570"
+accountCity = "jaipur"
+
+// accountId = 2
+
+accountEmail = "nc@gmail.com"
+accountPassword = "982950"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
