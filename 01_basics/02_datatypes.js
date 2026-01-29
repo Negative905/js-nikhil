e5fd7d89efe3d83d9592726edde7c49153d312c7
Nikhil Chauhan <nikhilchauhan6619@gmail.com>
2026-01-29 18:57:41 +0000

    02 datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treats all JS code as newer version
+
+// alert ( 3 + 3 ), we are using nodeJS not browser
+
+console.log("negative");
+
+let name = "negative"
+let age = 22
+let isLoggedIn = false
+
+// number ( 2 to power 53)
+// bigint (huge no.s)
+// string => ""
+// boolean => (true/false)
+// null => (standalone value)
+// undefined
+// symbol => (uniqueness)
+
+// object
+
+console.log(typeof "negative");
